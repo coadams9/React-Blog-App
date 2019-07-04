c5422724e756a13c37d4519e9a062fc4654b4c8b
Cory <coadams9@gmail.com>
2019-07-03 20:44:39 -0400

    initial commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,10 @@
+import { React } from 'react'
+
+
+function HomePage() {
+    return (
+        <div>
+            <h2>HI</h2>
+        </div>
+    )
+}
---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+
+
+const rootReducer = combineReducers({
+
+})
+
+export default createStore(rootReducer, applyMiddleware(thunk));
