150c0e6fef4b8dc05a3ce01ddf8fc7b9a7511bb3
Cory <coadams9@gmail.com>
2019-07-03 21:02:09 -0400

    downloaded all libraries and working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import HomePage from './components/HomePage'
 
 
--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import Store from './store';
 import { BrowserRouter as Router } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import Store from './store'
 
 ReactDOM.render(<Provider store={Store}>
     <Router>
         <App />
     </Router>
-</Provider>, document.getElementById('root'))
-
+</Provider>, document.getElementById('root'));
